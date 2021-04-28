c1a3b857ba4a2921f9278c0979cac9bdfc15146f
Eugen Burianov <eugenburianov@gmail.com>
2021-04-28 11:43:43 -0500

    type classes with implicits

--- src/main/scala/ch1_typeclasses/Json.scala
+++ src/main/scala/ch1_typeclasses/Json.scala
@@ -1,5 +1,29 @@
 package ch1_typeclasses
 
-trait Json {
+sealed trait Json
 
+final case class JsObject(get: Map[String, Json]) extends Json
+final case class JsString(get: String) extends Json
+final case class JsNumber(get: Double) extends Json
+final case object JsNull extends Json
+
+//The serialize to JSON behaviour
+trait JsonWriter[A] {
+  def write(value: A): Json
+}
+
+/*
+* Scala language constructs correspond to the components of type classes as follows:
+• traits: type classes;
+• implicit values: type class instances;
+• implicit parameters: type class use; and
+• implicit classes: optional utilities that make type classes easier to use.
+* */
+
+/* Cats uses type class patterns: Interface Objects and Interface Syntax */
+
+//Interface Object (place methods in singleton object
+object Json {
+  def toJson[A](value: A)(implicit w: JsonWriter[A]): Json =
+    w.write(value)
 }
