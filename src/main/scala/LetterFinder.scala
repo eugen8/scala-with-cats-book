d0435f935d90d0e4ebae7d730debc82b39e8c529
Eugen Burianov <eugenburianov@gmail.com>
2021-08-25 14:41:06 -0500

    reordering of the functions

--- src/main/scala/LetterFinder.scala
+++ src/main/scala/LetterFinder.scala
@@ -1,30 +1,6 @@
 import scala.collection.mutable.{Map => MutMap}
 
 object LetterFinder extends App {
-
-  println(findUnique("abcabd") == "c")
-  println(findUnique("a") == "a")
-  println(findUnique("acbcabd") == "d")
-  println(findUnique("") == "")
-
-
-  //Complexity O(n^2)
-  def findUnique(text: String): String =
-    text.foldLeft((false, ""))((agg, el) => {
-      agg match {
-        case (false, _) =>
-          (text.indexOf(el) == text.lastIndexOf(el), el.toString)
-        case _ => agg
-      }
-    })._2
-
-
-  println(findBetterUnique("abcabd") == Some("c"))
-  println(findBetterUnique("a") == Some("a"))
-  println(findBetterUnique("acbcabd") == Some("d"))
-  println(findBetterUnique("acbcdabcd") == None)
-  println(findBetterUnique("") == None)
-
   //This has O(n) complexity. Still not completely satisfied with mutable map, will think of a better way
   //make it in a more functional style
   def findBetterUnique(text: String): Option[String] = {
@@ -37,5 +13,29 @@ object LetterFinder extends App {
     })
     text.find(l => charsMap.get(l).contains(1)).map(_.toString) //or could have left it as char
   }
+  
+  println(findBetterUnique("abcabd") == Some("c"))
+  println(findBetterUnique("a") == Some("a"))
+  println(findBetterUnique("acbcabd") == Some("d"))
+  println(findBetterUnique("acbcdabcd") == None)
+  println(findBetterUnique("") == None)
+  
+  
+  //Initial, more naive implementation, complexity O(n^2)
+  def findUnique(text: String): String =
+    text.foldLeft((false, ""))((agg, el) => {
+      agg match {
+        case (false, _) =>
+          (text.indexOf(el) == text.lastIndexOf(el), el.toString)
+        case _ => agg
+      }
+    })._2
 
+
+  println(findUnique("abcabd") == "c")
+  println(findUnique("a") == "a")
+  println(findUnique("acbcabd") == "d")
+  println(findUnique("") == "")
+  
+  // a text.find(...) implementation would be a much simpler solution than findUnique here, but still with O(n^2) complexity.
 }
